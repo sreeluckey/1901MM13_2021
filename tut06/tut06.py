cbd4d2811cf95f2bb352b29fe26862e985bbf609
sreeluckey <88873626+sreeluckey@users.noreply.github.com>
2021-10-27 21:05:01 +0530

    Update tut06.py
    sreekanth_1901mm13

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -1,3 +1,5 @@
+
+
 def regex_renamer():
 
 	# Taking input from the user
@@ -9,7 +11,28 @@ def regex_renamer():
 	webseries_num = int(input("Enter the number of the web series that you wish to rename. 1/2/3: "))
 	season_padding = int(input("Enter the Season Number Padding: "))
 	episode_padding = int(input("Enter the Episode Number Padding: "))
-
-
+     
+	import re
+	import os
+	import shutil
+	dictn={1:"Breaking Bad",2:"Game of Thrones",3:"Lucifer"}
+	try:
+		shutil.rmtree(f"corrected_srt\\{dictn[webseries_num]}")
+	except:
+		pass
+	shutil.copytree(f"wrong_srt\\{dictn[webseries_num]}",f"corrected_srt\\{dictn[webseries_num]}")
+	files_list=os.listdir(f"corrected_srt\\{dictn[webseries_num]}")	
+	
+	for file_name in files_list:
+		nums_list = re.findall(r'\d+', file_name)
+		words_list=re.split(r'\.', file_name)
+		ep_name=words_list[0].split("-")
+		if(webseries_num == 1):
+			os.rename(f"corrected_srt\\{dictn[webseries_num]}\\{file_name}", f"corrected_srt\\{dictn[webseries_num]}\\{dictn[webseries_num]} Season {nums_list[0].zfill(season_padding)} Episode {nums_list[1].zfill(episode_padding)}.{words_list[-1]}")
+		elif(webseries_num == 2): 
+		    os.rename(f"corrected_srt\\{dictn[webseries_num]}\\{file_name}", f"corrected_srt\\{dictn[webseries_num]}\\{dictn[webseries_num]} - Season {nums_list[0].zfill(season_padding)} Episode {nums_list[1].zfill(episode_padding)} -{ep_name[-1]}.{words_list[-1]}")
+		elif(webseries_num == 3): 
+		    os.rename(f"corrected_srt\\{dictn[webseries_num]}\\{file_name}", f"corrected_srt\\{dictn[webseries_num]}\\{dictn[webseries_num]} - Season {nums_list[0].zfill(season_padding)} Episode {nums_list[1].zfill(episode_padding)} -{ep_name[-1]}.{words_list[-1]}")
 
 regex_renamer()
+
